1924f3051e4148e4560f12d23aca1b7e73d5b12b
Miguel Mejía Hernández <beatlesmiguel286@gmail.com>
2024-03-01 12:25:47 -0600

    init project pokemon

---
+++ src/main/java/com/example/pokemonmiguel/PokemonmiguelApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokemonmiguel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonmiguelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonmiguelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pokemonmiguel/PokemonmiguelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokemonmiguel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonmiguelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
